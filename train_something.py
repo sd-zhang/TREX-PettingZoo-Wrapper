5647fa77de706668a730e339c44b451d8fdc5847
Daniel May <dcmay@ualberta.ca>
2023-10-05 21:58:22 -0600

    It might be this easy

--- train_something.py
+++ train_something.py
@@ -3,6 +3,7 @@ import numpy as np
 import pettingzoo as pz
 import stable_baselines3 as sb3
 import supersuit as ss
+from supersuit.vector.sb3_vector_wrapper import SB3VecEnvWrapper
 from pettingzoo.test import parallel_api_test
 
 if "__main__" == __name__:  # this is needed to make sure the code is not executed when imported
@@ -14,11 +15,16 @@ if "__main__" == __name__:  # this is needed to make sure the code is not execut
     trex_env = TrexEnv(config_name=config_name, **kwargs)
 
     trex_env = ss.pettingzoo_env_to_vec_env_v1(trex_env)
+    trex_env = SB3VecEnvWrapper(trex_env)
+    # trex_env = ss.concat_vec_envs_v1(trex_env, 1, base_class="stable_baselines3")
 
-    model = sb3.PPO(sb3.ppo.MlpPolicy,
+
+    # trex_env = ss.vector.markov_vector_wrapper.MarkovVectorEnv(trex_env)
+
+    model = sb3.ppo.PPO("MlpPolicy",
         trex_env,
-        n_steps=40,
-        verbose=2,
-        batch_size=64,
+        device="gpu",
+        n_steps=8,
+        verbose=1,
     )
     model.learn(total_timesteps=10000)
